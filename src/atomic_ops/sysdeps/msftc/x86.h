bba1ab791e7128edcb724ba83b4acfe4bd8e0e81
hboehm <hboehm>
2008-01-08 01:51:12 +0000

    2008-01-08 Hans Boehm <Hans.Boehm@hp.com> (Really mostly Joerg Wagner) 	* src/atomic_ops/sysdeps/msftc/x86.h: Conditionally add 	compare_double_and_swap_double.

--- src/atomic_ops/sysdeps/msftc/x86.h
+++ src/atomic_ops/sysdeps/msftc/x86.h
@@ -20,8 +20,10 @@
  * SOFTWARE. 
  */
 
-/* The following really assume we have a 486 or better. */
+/* The following really assume we have a 486 or better. 		*/
 /* If ASSUME_WINDOWS98 is defined, we assume Windows 98 or newer.	*/
+/* If ASSUME_VISTA is defined, we assume Windows Server 2003, Vista	*/
+/* or later.								*/
 
 #include "../all_aligned_atomic_load_store.h"
 
@@ -159,5 +161,37 @@ AO_compare_and_swap_full(volatile AO_t *addr,
 #ifdef _WIN64
 #  error wrong architecture
 #endif
+
+#ifdef ASSUME_VISTA
+/* NEC LE-IT: whenever we run on a pentium class machine we have that
+ * certain function */
+
+#include "../standard_ao_double_t.h"
+#pragma intrinsic (_InterlockedCompareExchange64)
+/* Returns nonzero if the comparison succeeded. */
+AO_INLINE int
+AO_compare_double_and_swap_double_full(volatile AO_double_t *addr,
+        			       AO_t old_val1, AO_t old_val2,
+               			       AO_t new_val1, AO_t new_val2) 
+{
+    __int64 oldv = (__int64)old_val2 | ((__int64)old_val1 << 32);
+    __int64 newv = (__int64)new_val2 | ((__int64)new_val1 << 32);
+    return _InterlockedCompareExchange64((__int64 volatile *)addr,
+                                       newv, oldv) == oldv;
+}
+#define AO_HAVE_compare_double_and_swap_double_full
+
+#ifdef __cplusplus
+AO_INLINE int
+AO_compare_double_and_swap_double_full(volatile AO_double_t *addr,
+		  		       AO_double_t old_val,
+				       AO_double_t new_val) 
+{
+    return _InterlockedCompareExchange64((__int64 volatile *)addr,
+		new_val.AO_whole, old_val.AO_whole) == old_val.AO_whole;
+}
+#define AO_HAVE_double_compare_and_swap_full
+#endif // __cplusplus
+#endif /* ASSUME_VISTA */
 
 #include "../ao_t_is_int.h"
