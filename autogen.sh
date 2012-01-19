125e5b247434f618a42502e97274e7672a2bd0ca
Bruno Haible <bruno@clisp.org>
2012-01-19 11:07:19 +0400

    Add autogen.sh file

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script creates (or regenerates) configure (as well as aclocal.m4,
+# config.h.in, Makefile.in, etc.) missing in the source repository.
+
+autoreconf -i
+
+echo
+echo "Ready to run './configure'."
