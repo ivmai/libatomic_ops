81065aaa7988015f29b67b69a7411296cb8f632d
Ivan Maidanski <ivmai@mail.ru>
2012-10-02 11:12:24 +0400

    Fix compare_double_and_swap_double_full definition condition in emul_cas
    * src/atomic_ops/sysdeps/emul_cas.h
    (AO_compare_double_and_swap_double_full): Test (define if)
    AO_HAVE_compare_double_and_swap_double_full macro instead of
    AO_HAVE_compare_double_and_swap_double.

--- src/atomic_ops/sysdeps/emul_cas.h
+++ src/atomic_ops/sysdeps/emul_cas.h
@@ -56,7 +56,7 @@ void AO_store_full_emulation(volatile AO_t *addr, AO_t val);
        AO_compare_and_swap_emulation(addr, old, newval)
 #define AO_HAVE_compare_and_swap_full
 
-#ifndef AO_HAVE_compare_double_and_swap_double
+#ifndef AO_HAVE_compare_double_and_swap_double_full
 # define AO_compare_double_and_swap_double_full(addr, old1, old2, \
                                                 newval1, newval2) \
         AO_compare_double_and_swap_double_emulation(addr, old1, old2, \
