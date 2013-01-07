744b11d87f8b6c477b41787775f1fa568951061c
Ivan Maidanski <ivmai@mail.ru>
2013-01-07 18:26:01 +0400

    Generate ordered_loads/stores_only headers from templates (code refactoring)
    * src/Makefile.am (EXTRA_DIST): Add ordered_loads_only.template,
    ordered_stores_only.template entries.
    * src/Makefile.am (BUILT_SOURCES): Add ordered_loads_only.h,
    ordered_stores_only.h entries (move from nobase_private_HEADERS).
    * src/Makefile.am (atomic_ops/sysdeps/loadstore/ordered_loads_only.h,
    atomic_ops/sysdeps/loadstore/ordered_stores_only.h): New rule (to
    generate header from the corresponding template).
    * src/atomic_ops/sysdeps/loadstore/ordered_loads_only.h: Regenerate
    (from the corresponding template).
    * src/atomic_ops/sysdeps/loadstore/ordered_stores_only.h: Likewise.
    * src/atomic_ops/sysdeps/loadstore/ordered_loads_only.template: New
    template header (code copied from the corresponding header of
    "loadstore" folder and parameterized with XSIZE and XCTYPE).
    * src/atomic_ops/sysdeps/loadstore/ordered_stores_only.template:
    Likewise.

--- src/atomic_ops/sysdeps/loadstore/ordered_stores_only.h
+++ src/atomic_ops/sysdeps/loadstore/ordered_stores_only.h
@@ -22,7 +22,7 @@
 
 #if defined(AO_HAVE_char_store)
   AO_INLINE void
-  AO_char_store_write(volatile unsigned char *addr, unsigned char val)
+  AO_char_store_write(volatile unsigned/**/char *addr, unsigned/**/char val)
   {
     AO_compiler_barrier();
     AO_char_store(addr, val);
@@ -32,10 +32,31 @@
 # define AO_char_store_release(addr, val) AO_char_store_write(addr, val)
 # define AO_HAVE_char_store_release
 #endif /* AO_HAVE_char_store */
+/*
+ * Copyright (c) 2003 by Hewlett-Packard Company.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #if defined(AO_HAVE_short_store)
   AO_INLINE void
-  AO_short_store_write(volatile unsigned short *addr, unsigned short val)
+  AO_short_store_write(volatile unsigned/**/short *addr, unsigned/**/short val)
   {
     AO_compiler_barrier();
     AO_short_store(addr, val);
@@ -45,10 +66,31 @@
 # define AO_short_store_release(addr, val) AO_short_store_write(addr, val)
 # define AO_HAVE_short_store_release
 #endif /* AO_HAVE_short_store */
+/*
+ * Copyright (c) 2003 by Hewlett-Packard Company.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #if defined(AO_HAVE_int_store)
   AO_INLINE void
-  AO_int_store_write(volatile unsigned int *addr, unsigned int val)
+  AO_int_store_write(volatile unsigned *addr, unsigned val)
   {
     AO_compiler_barrier();
     AO_int_store(addr, val);
@@ -58,6 +100,27 @@
 # define AO_int_store_release(addr, val) AO_int_store_write(addr, val)
 # define AO_HAVE_int_store_release
 #endif /* AO_HAVE_int_store */
+/*
+ * Copyright (c) 2003 by Hewlett-Packard Company.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #if defined(AO_HAVE_store)
   AO_INLINE void
