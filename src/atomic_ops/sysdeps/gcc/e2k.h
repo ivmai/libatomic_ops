f7865a491fa9597da39eecb9c127859a1867ba3d
Ivan Maidanski <ivmai@mail.ru>
2023-10-25 08:23:49 +0300

    Support double-wide primitives on E2K v5+ in 64-bit mode (supersede commits ca5d76e62, e74443345)
    Issue #59 (libatomic_ops)
    
    This requires CPU elbrus-v5 or later.
    
    * src/atomic_ops/sysdeps/gcc/e2k.h: Replace defined(__ptr32__) to
    __SIZEOF_SIZE_T__==4.
    * src/atomic_ops/sysdeps/gcc/e2k.h [__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16
    && !__clang__]: Define AO_GCC_HAVE_double_SYNC_CAS and include
    standard_ao_double_t.h.
    * src/atomic_ops/sysdeps/gcc/e2k.h [!__ptr32__
    && AO_PREFER_BUILTIN_ATOMICS]: Define AO_GCC_HAVE_double_SYNC_CAS and
    include standard_ao_double_t.h only if __clang__ and __iset__>=5;
    add comment; remove TODO item.

--- src/atomic_ops/sysdeps/gcc/e2k.h
+++ src/atomic_ops/sysdeps/gcc/e2k.h
@@ -23,13 +23,15 @@
 /* As of clang-9, all __GCC_HAVE_SYNC_COMPARE_AND_SWAP_n are missing.   */
 #define AO_GCC_FORCE_HAVE_CAS
 
-#if defined(__ptr32__) || defined(AO_PREFER_BUILTIN_ATOMICS)
+#if (__SIZEOF_SIZE_T__ == 4) \
+    || (defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_16) && !defined(__clang__)) \
+    || (defined(__clang__) && __iset__ >= 5 /* elbrus-v5 or later */ \
+        && defined(AO_PREFER_BUILTIN_ATOMICS))
+  /* Note for 128-bit operations: clang reports warning                 */
+  /* "large atomic operation may incur significant performance penalty" */
+  /* and requires LDFLAGS="-latomic", thus this is not on by default.   */
 # define AO_GCC_HAVE_double_SYNC_CAS
 # include "../standard_ao_double_t.h"
-#else
-  /* TODO: This is not enabled for the 64-bit mode by default because,  */
-  /* as of lcc-1.26.20, this requires -latomic and also leads to        */
-  /* test_stack failure.                                                */
 #endif
 
 #include "generic.h"
