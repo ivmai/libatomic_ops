0cd408713432b00d5971f1cb5c0d9ffb4a655492
Ivan Maidanski <ivmai@mail.ru>
2023-12-15 22:04:39 +0300

    [7.4.20]
    Bump libatomic_ops version to 7.4.20
    
    * ChangeLog (7.4.20): Set release date.
    * README.md: Bump micro version.
    * configure.ac (AC_INIT): Likewise.
    * src/atomic_ops/ao_version.h (AO_VERSION_MICRO): Likewise.

--- src/atomic_ops/ao_version.h
+++ src/atomic_ops/ao_version.h
@@ -34,4 +34,4 @@
 /* The version here should match that in configure.ac and README.       */
 #define AO_VERSION_MAJOR 7
 #define AO_VERSION_MINOR 4
-#define AO_VERSION_MICRO 18 /* 7.4.18 */
+#define AO_VERSION_MICRO 20 /* 7.4.20 */
