c9ebc1683c0f0154fa8e1db1bad258a073bce8de
Bruno Haible <bruno@clisp.org>
2024-01-26 22:07:22 +0100

    Fix 'libtool library used but LIBTOOL is undefined' autoreconf error
    Install libtool.m4 and ltmain.sh in the build tree before running
    autoreconf.  This is needed if autoreconf and libtoolize are available
    from the different directories of $PATH.
    
    * autogen.sh: Invoke libtoolize; add comment.

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,10 @@ set -e
 # This script creates (or regenerates) configure (as well as aclocal.m4,
 # config.h.in, Makefile.in, etc.) missing in the source repository.
 
+# Install libtool.m4 and ltmain.sh in the build tree.  This command is needed
+# if autoreconf and libtoolize are available from the different directories.
+libtoolize -i -c
+
 autoreconf -i
 
 echo
